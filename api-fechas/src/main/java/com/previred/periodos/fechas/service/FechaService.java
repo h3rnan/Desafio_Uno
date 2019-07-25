749789229aa059b39592a6cbed359136125ee026
Hernan Aja <hernan.aja@imagemaker.cl>
2019-07-24 21:28:54 -0400

    Se agrega service

---
+++ api-fechas/src/main/java/com/previred/periodos/fechas/service/FechaService.java
@@ -0,0 +1,14 @@
+package com.previred.periodos.fechas.service;
+
+import com.previred.periodos.fechas.model.Periodo;
+import com.previred.periodos.fechas.model.PeriodoSolucion;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FechaService {
+
+    public PeriodoSolucion getPeriodoSolucion(Periodo periodo) {
+        return null;
+    }
+
+}
